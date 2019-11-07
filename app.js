ab02b87fc93727242c9f663f6edb8859f9ef1b19
Emil Dimitrov <edimitrov1@gmail.com>
2019-11-07 13:49:55 +0200

    Name some relevent DOM query strings and include them in a object for ease of use

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const DataModule = (() => {
 })();
 
 const UIModule = (() => {
+  const DOMstrings = {
+    gameButton: '.btn',
+    playerOneName: '.player-1-field',
+    playerTwoName: '.player-2-field'
+  };
 
+  return {
+    getDOMstrings() {
+      return DOMstrings;
+    }
+  };
 })();
 
 const Controller = ((Data, UI) => {
