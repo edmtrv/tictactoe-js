96336492cf21a2dcf1ce88f3586db39c01fe9239
Emil Dimitrov <edimitrov1@gmail.com>
2019-11-09 00:26:52 +0200

    Add all possible winning combos to the board factory

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const DataModule = (() => {
 
   const Board = () => {
     const board = [null, null, null, null, null, null, null, null, null];
+    const winCombos = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6]
+    ];
     const markCell = (pos, symbol) => {
       board[pos] = symbol;
     };
