fd89f70a7b727344fd9241d10a8e7c1c5df66a12
Emil Dimitrov <edimitrov1@gmail.com>
2019-11-09 15:05:35 +0200

    Add a method that checks if the board is full

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const DataModule = (() => {
       [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8],
       [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6]
     ];
+
+    const isFull = () => {
+      !board.some(pos => pos === null);
+    };
+
     const markCell = (pos, symbol) => {
       board[pos] = symbol;
     };
