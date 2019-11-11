ec53a4f248f30da8274c56e1e8f135ea31104214
Emil Dimitrov <edimitrov1@gmail.com>
2019-11-11 14:56:33 +0200

    Pass a computed symbol to check if game is over to prevent being affected by player switch

--- app.js
+++ app.js
@@ -14,9 +14,10 @@ const DataModule = (() => {
       [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6]
     ];
 
+    const getGrid = () => grid;
+
     const positionsBySymbol = (symbol) => {
       const positions = [];
-
       grid.forEach((mark, pos) => {
         if (mark === symbol) {
           positions.push(pos)
@@ -26,9 +27,7 @@ const DataModule = (() => {
       return positions;
     };
 
-    const isFull = () => {
-      !grid.some(pos => pos === null);
-    };
+    const isFull = () => !grid.some(pos => pos === null);
 
     const isWon = (currentSymbol) => {
       const positions = positionsBySymbol(currentSymbol);
@@ -45,40 +44,34 @@ const DataModule = (() => {
       grid[pos] = symbol;
     };
 
-    const isEmptyCell = (pos) => {
-      return !grid[pos];
-    };
+    const isEmptyCell = (pos) => !grid[pos];
 
-    return {markCell, isEmptyCell, isFull, isWon};
+    return {markCell, isEmptyCell, isFull, isWon, getGrid};
   };
 
   const Game = (board, ...players) => {
+    const getBoard = () => board.getGrid();
     const switchActivePlayer = () => players.reverse();
 
     const getActivePlayer = () => players[0];
 
-    const isGameOver = () => {
-      const symbol = getActivePlayer().getSymbol();
-      board.isWon(symbol) || board.isFull()
+    const isGameOver = (bySymbol) => {
+      return board.isWon(bySymbol) || board.isFull()
     };
 
     const getWinner = () => board.isWon() && players[0];
 
     const turn = (position) => {
-      const currentPlayer = getActivePlayer();
+      if (!board.isEmptyCell(position)) return;
 
-      if (!board.isEmptyCell(position)) {
-        return;
-      }
-
-      board.markCell(position, currentPlayer.getSymbol());
+      board.markCell(position, getActivePlayer().getSymbol());
 
       switchActivePlayer();
 
       return position;
     };
 
-    return {turn, isGameOver, getWinner, getActivePlayer};
+    return {turn, isGameOver, getWinner, getActivePlayer, getBoard};
   };
 
   return {Player, Board, Game};
@@ -110,16 +103,19 @@ const Controller = ((Data, UI) => {
 
     document.querySelector(DOM.board).addEventListener('click', e => {
       const clickedCell = +e.target.dataset.cell;
+      const activePlayer = game.getActivePlayer();
 
       if (clickedCell === undefined) return;
 
       const markedPosition = game.turn(clickedCell);
 
       if (markedPosition !== undefined) {
-        console.log(game.getActivePlayer().getSymbol());
+        console.log(activePlayer.getSymbol());
       }
-
-      console.log(game.isGameOver());
+      console.log(game.getBoard());
+      if (game.isGameOver(activePlayer.getSymbol())) {
+        console.log('game over');
+      }
     });
   }
 
